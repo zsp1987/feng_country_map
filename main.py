b2ff7877cb6152ea64d8d65cce5d2a1059592b48
Shaopeng Zhang <zsp1987@gmail.com>
2021-09-04 10:17:40 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+def standerize(name: str) -> str:
+    return None
+
+def isChina(country_code: str) -> bool:
+    return False
+
+def isIndia(country_code: str) -> bool:
+    return False
+
+def isMiddleEast(country_code: str) -> bool:
+    return False
+
+def isAmerica(country_code: str) -> bool:
+    return False
+
+def isEurope(country_code: str) -> bool:
+    return False
+
+def draw_map():
+    pass
+
+if __name__ == '__main__':
+    draw_map()
